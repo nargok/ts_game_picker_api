0c4ccbc7a97c48292a537d31fe1632647d26f382
nargok <nargok@gmail.com>
2021-02-06 14:36:41 +0900

    postgres docker settings

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,14 +6,14 @@ router.get('/helloworld', (req, res) => {
   res.status(200).send({ message: 'Hello, world'})
 })
 
-app.use((req, res) => {
-  res.status(404)
-  res.render('error', {
-    param: {
-      status: 404,
-      message: 'not found'
-    }
-  })
-})
+// app.use((req, res) => {
+//   res.status(404)
+//   res.render('error', {
+//     param: {
+//       status: 404,
+//       message: 'not found'
+//     }
+//   })
+// })
 
 module.exports = router;
