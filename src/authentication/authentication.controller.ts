b36d107c3194c9866a8ae7ef00a1ac2bb7941ec6
nargok <nargok@gmail.com>
2021-02-07 17:39:48 +0900

    create jwt

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,16 +1,19 @@
 import * as bcrypt from 'bcrypt';
 import * as express from 'express';
+import * as jwt from 'jsonwebtoken';
 import { getRepository } from 'typeorm';
 
 import Controller from '../interfaces/controller.interface'
-import userModel from '../user/user.entity'
+import UserModel from '../user/user.entity'
 import CreateUserDto from '../user/user.dto'
 import LogInDto from './login.dto'
+import TokenData from '../interfaces/tokenData.interface'
+import DataStoredInToken from '../interfaces/dataStoredInToken'
 
 class AuthenticationController implements Controller {
   public path = '/auth'
   public router = express.Router()
-  private userRepository = getRepository(userModel)
+  private userRepository = getRepository(UserModel)
 
   constructor() {
     this.initializeRoutes()
@@ -20,7 +23,7 @@ class AuthenticationController implements Controller {
     this.router.post(`${this.path}/register`, this.registration)
   }
 
-  private registration = async(request: express.Request, response: express.Response, next: express.NextFunction) {
+  private registration = async(request: express.Request, response: express.Response, next: express.NextFunction) => {
     const userData: CreateUserDto = request.body
     if (await this.userRepository.findOne({ email: userData.email })) {
       // TODO 後で例外処理に切り替える
@@ -33,11 +36,13 @@ class AuthenticationController implements Controller {
       })
       await this.userRepository.save(user)
       user.password = ''
+      const tokenData = this.createToken(user)
+      response.setHeader('Set-Cookie', [this.createCookie(tokenData)])
       response.send(user)
     }
   }
 
-  private loggingIn = async(request: express.Request, response: express.Response, next: express.NextFunction) {
+  private loggingIn = async(request: express.Request, response: express.Response, next: express.NextFunction) => {
     const logInData: LogInDto = request.body
     const user = await this.userRepository.findOne({ email: logInData.email })
     if (user) {
@@ -53,6 +58,23 @@ class AuthenticationController implements Controller {
       throw Error('Wrong credeitials')
     }
   }
+
+  private createCookie(tokenData: TokenData) {
+    return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn}`
+  }
+
+  private createToken(user: UserModel): TokenData {
+    const expiresIn = 60 * 60
+    const secret = process.env.JWT_SECRET!
+    const dataStoredInToken: DataStoredInToken = {
+      id: user.id!
+    }
+    return {
+      expiresIn,
+      token: jwt.sign(dataStoredInToken, secret, { expiresIn })
+    }
+  }
+
 }
 
 export default AuthenticationController;
